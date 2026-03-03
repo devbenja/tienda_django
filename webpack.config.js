198d81cc5eaa6911ff59d34d273138d30000ad60
devbenja <cariasbenjamin59@gmail.com>
2026-03-02 21:25:43 -0600

    feat: Set up initial SCSS styling, basic HTML templates, and Webpack asset compilation.

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,43 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-module.exports = {
-    entry: {
-        main: ['./assets/scss/main.scss', './assets/js/main.js']
-    },
-    output: {
-        path: path.resolve(__dirname, 'static'),
-        filename: 'js/[name].[contenthash].js',
-        clean: true
-    },
-    module: {
-        rules: [
-            {
-                test: /\.scss$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    'css-loader',
-                    'sass-loader'
-                ]
-            }
-        ]
-    },
-    plugins: [
-        new MiniCssExtractPlugin({
-            filename: 'css/[name].[contenthash].css'
-        })
-    ],
-    // Desactivar source maps en desarrollo para simplicidad
-    devtool: false
+module.exports = (env, argv) => {
+    const isProduction = argv.mode === 'production';
+
+    return {
+        entry: {
+            main: ['./assets/scss/main.scss', './assets/js/main.js']
+        },
+        output: {
+            path: path.resolve(__dirname, 'static'),
+            filename: isProduction ? 'js/[name].[contenthash].js' : 'js/[name].js',
+            clean: true
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.scss$/,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        'css-loader',
+                        {
+                            loader: 'sass-loader',
+                            options: {
+                                api: 'modern-compiler',
+                                sassOptions: {
+                                    silenceDeprecations: ['legacy-js-api', 'import', 'color-functions']
+                                }
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new MiniCssExtractPlugin({
+                filename: isProduction ? 'css/[name].[contenthash].css' : 'css/[name].css'
+            })
+        ],
+        devtool: false
+    };
 };
